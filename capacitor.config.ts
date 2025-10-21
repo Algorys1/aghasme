f98eba7ad3d843d0d3c2cde5e2675035a85d699c
Matthieu Estrada <Matthieu.Estrada@etap.com>
2025-10-21 17:56:32 +0200

    Prepare android export

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aghasme.game',
+  appName: 'aghasme',
+  webDir: 'dist/aghasme/browser'
+};
+
+export default config;
